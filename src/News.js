f2d9999d81f9f60b972bb85280e520a8c0440dc5
shankeybharadwaj <66555439+shankeybharadwaj@users.noreply.github.com>
2022-06-02 01:10:29 +0530

    my commit

--- src/News.js
+++ src/News.js
@@ -6,42 +6,6 @@ import PropTypes from 'prop-types'
 
 export class News extends Component {
 
-  myArticles = {
-    "status": "ok",
-    "totalResults": 3,
-    "articles": [
-      {
-        "source": { "id": "cnn", "name": "CNN" },
-        "author": "Kevin Liptak and Arlette Saenz, CNN",
-        "title": "Bidens visit site of devastating Uvalde school shooting - CNN",
-        "description": "President Joe Biden on Sunday faces the grimly familiar task of comforting families after another mass shooting, this time at an elementary school in Texas, as a weary nation grapples with an endless spate of gun violence.",
-        "url": "https://www.cnn.com/2022/05/29/politics/joe-biden-uvalde-texas-visit/index.html",
-        "urlToImage": "https://cdn.cnn.com/cnnnext/dam/assets/220529124327-05-biden-uvalde-texas-0529-super-tease.jpg",
-        "publishedAt": "2022-05-29T18:05:00Z",
-        "content": "(CNN)President Joe Biden on Sunday faces the grimly familiar task of comforting families after another mass shooting, this time at an elementary school in Texas, as a weary nation grapples with an en… [+5977 chars]"
-      },
-      {
-        "source": { "id": "cnn", "name": "CNN" },
-        "author": "Ray Sanchez, CNN",
-        "title": "'We're in trouble.' 80 minutes of horror at Robb Elementary School - CNN",
-        "description": "Two days before the end of the academic year the students of Robb Elementary School wore their Sunday best for a celebration. Within hours, they would cross paths with a high school dropout who one week earlier had legally purchased two AR-15 style rifles for…",
-        "url": "https://www.cnn.com/2022/05/29/us/uvalde-texas-elementary-school-shooting-week/index.html",
-        "urlToImage": "https://cdn.cnn.com/cnnnext/dam/assets/220527195351-uvalde-robb-elementary-school-052722-super-tease.jpg",
-        "publishedAt": "2022-05-29T14:27:00Z",
-        "content": "This account of events surrounding the massacre at Robb Elementary School in Uvalde, Texas, is based on official statements, interviews with parents, witnesses, law enforcement and local officials, a… [+14387 chars]"
-      },
-      {
-        "source": { "id": null, "name": "YouTube" },
-        "author": null,
-        "title": "Texas officials had contradictory accounts of Uvalde school shooting | USA TODAY - USA TODAY",
-        "description": "Texas officials, including Governor Greg Abbott, offered contradictory narratives over two days after the school shooting in Uvalde that left 19 students and...",
-        "url": "https://www.youtube.com/watch?v=M2DNXZlGqjo",
-        "urlToImage": "https://i.ytimg.com/vi/M2DNXZlGqjo/hqdefault.jpg",
-        "publishedAt": "2022-05-28T17:05:35Z",
-        "content": null
-      }
-    ]
-  }
   
 
   static defaultProps = {
